077bdf742d0990eca6036c9db9efd534b79e21d1
omfuke <46218908+omfuke@users.noreply.github.com>
2019-03-23 01:52:25 +0530

    Create Frozen-Lake.py

---
+++ Frozen-Lake.py
@@ -0,0 +1,103 @@
+import gym
+import random
+import time
+
+
+from IPython.display import clear_output
+import numpy as np
+
+#env is enviorenment object
+
+env = gym.make('FrozenLake-v0')
+action_space_size=env.action_space.n
+state_space_size=env.observation_space.n
+
+#Q-Table
+q_table=np.zeros((state_space_size,action_space_size))
+
+#variables
+num_episodes=10000
+max_steps_per_episode=100
+learning_rate=0.1
+discount_rate=0.99
+exploration_rate=1
+max_exploration_rate=1
+min_exploration_rate=0.01
+exploration_decay_rate=0.001
+
+rewards_all_episodes=[]
+
+#Q-learning-Algorithm
+for episode in range(num_episodes):     #starting of new turn
+    state=env.reset()
+    done=False
+    rewards_current_episode=0
+
+    for step in range(max_steps_per_episode):   #step wise motion
+        #exploration-explotation
+        exploration_rate_threshold=random.uniform(0,1)
+        if exploration_rate_threshold > exploration_rate:
+            action=np.argmax(q_table[state,:])
+        else:
+            action=env.action_space.sample()
+
+
+        new_state , reward , done , info =env.step(action)
+
+        #Update Q-Table
+        q_table[state, action] = q_table[state, action] * (1 - learning_rate) + \
+                                 learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
+
+        state=new_state
+        rewards_current_episode=reward
+
+        if done==True:
+            break
+
+    #Exploration rate-decay
+    exploration_rate=min_exploration_rate+ (max_exploration_rate-min_exploration_rate)*np.exp(-exploration_decay_rate*episode)
+
+    rewards_all_episodes.append(rewards_current_episode)
+
+#print average per thousand episode
+rewards_per_thosand_episodes = np.split(np.array(rewards_all_episodes),num_episodes/1000)
+count = 1000
+
+for r in rewards_per_thosand_episodes:
+
+    print(count ,":", str(sum(r/1000)) )
+    count += 1000
+
+
+print(q_table)
+
+for episode in range(3):
+    state = env.reset()
+    done=False
+    print("*******EPISODE: ",episode+1,"*******\n\n\n\n")
+    time.sleep(1)
+
+
+    for step in range(max_steps_per_episode):
+        clear_output(wait=True)
+        env.render()
+        time.sleep(0.3)
+
+        action = np.argmax(q_table[state, :])
+        new_state, reward, done, info = env.step(action)
+
+        if done:
+            clear_output(wait=True)
+            env.render()
+            if reward == 1:
+                print("****You reached the goal!****")
+                time.sleep(3)
+            else:
+                print("****You fell through a hole!****")
+                time.sleep(3)
+                clear_output(wait=True)
+            break
+
+        state=new_state
+
+env.close()
